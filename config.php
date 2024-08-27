80c7c5ede74587f87b98041ed314bb8039a1c9e7
Athalah Rafif Irsyach Sarbrani <sarbrani.athallah@gmail.com>
2024-08-27 14:39:27 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'u1569497_lms';
+$CFG->dbuser    = 'u1569497_lms';
+$CFG->dbpass    = '&y?z@^uB*yTk';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'https://lms.cpsrg.org';
+$CFG->dataroot  = '/home/u1569497/public_html/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
